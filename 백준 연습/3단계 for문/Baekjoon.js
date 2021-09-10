4871f59922ca31a171d7867a38778bb1b6ff8c80
gosmain <gosmain@naver.com>
2021-09-10 21:55:02 +0900

    B

--- 백준 연습/3단계 for문/Baekjoon.js
+++ 백준 연습/3단계 for문/Baekjoon.js
@@ -1,26 +1,29 @@
 // 1번 (구구단)
-var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
+var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
 var num1 = parseInt(input[0]);
 
-    for (var i = 1; i <= 9; i++) {
-        var gugu = num1 * i;
-        console.log(num1 + "*" + i + "=" + gugu);
-    }
+  for (var i = 1; i <= 9; i++) {
+      console.log(num1 + " * " +i + " = " + num1 * i);
+  }
 
-    const fs = require("fs");
-let stdin =
-  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `2`;
+// 2번 (a+b-3)
+//틀린놈
+var input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
-let num1 = parseInt(stdin);
+var turn = parseInt(input[0]);
 
-for (var i = 1; i <= 9; i++) {
-    var gugu = num1 * i;
-    console.log(`${num1} * ${i} = ${gugu}`);
+for (var i = 1; i <= turn[0]; i++) {
+  var num1 = parseInt(input[1]);
+  var num2 = parseInt(input[2]);
+  console.log(num1+num2);
 }
+//맞는놈
+var input = require('fs').readFileSync('/dev/stdin').toString().split('\n'); //입력에 공백 = \n
 
-for (let i = 1; i <= 9; i++) {
-  let gogo = number2 * i;
-  console.log(`${number2} * ${i} = ${gogo}`);
+for (var i = 1; i <= input[0]; i++) {
+    var numbers = input[i].split(' ').map(Number);
+    
+    console.log(numbers[0] + numbers[1]);
 }
