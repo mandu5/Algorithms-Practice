77bfa7f0513e609a52befcb493984cd88eca9abe
gosmain <gosmain@naver.com>
2021-10-12 17:09:17 +0900

    BJ lv 9.6

--- 백준 연습/9단계 기본 수학 2/Baekjoon.js
+++ 백준 연습/9단계 기본 수학 2/Baekjoon.js
@@ -133,3 +133,49 @@ for (let i = 0; i < inputs.length; i ++) {
     }
     PrimeNumber();
 }
+
+
+// 6번 (골드바흐의 추측) <에라토스테네스의 체 사용>
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(n => Number(n));
+
+const n = 2;
+const m = 10000;
+
+let prime = [];
+for (let i = n; i <= m; i++) {
+    prime[i] = i;
+}
+for (let i = 2; i <= m; i++) {
+    if (prime[i] === 0) continue;
+    for (let j = i + i; j <= m; j += i) {
+        prime[j] = 0;
+    }
+}
+prime = prime.filter(n => n !== 0);
+
+
+for (let i = 0; i < input.length; i++) {               // 테스트케이스 하나씩 반복                                            
+    const num = Number(input[i]);
+    let answer = [];                                   // 가능한 골드바흐 파티션을 저장할 배열
+
+    for (let j = 0; prime[j] < num / 2 + 1; j++) {     // 주어진 수의 절반보다 작은 소수만 탐색하면 됨
+        const index = prime.indexOf(num - prime[j]);   // (주어진 수 - 소수)가 소수인지
+        if (index !== -1) {                            // 소수라면
+        answer.push([prime[j], prime[index]]);         // 골드바흐 파티션이므로 배열에 저장
+        }
+    }
+    if (answer.length !== 0) {                         // 골드바흐 파티션이 존재했다면
+        const a = answer.pop();                        // 가장 마지막 파티션을 가져옴 (두 소수의 차이가 가장 작음)
+        console.log(`${a[0]} ${a[1]}`);                // 출력
+    }
+}
+만약 가능한 n의 골드바흐 파티션이 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
+
+이 부분을 만족시키려고 하다보니 많은 시간이 소요되었다.
+
+처음에는 주어진 수 - 소수가 소수인지 판별한 뒤 소수라면 바로 출력하였는데, 그럴 경우 두 소수의 차이가 큰 것이 출력되는 문제가 발생하였다.
+
+가능한 골드바흐 파티션을 모두 구해 살펴보았더니, 10의 경우 [3, 7], [5, 5], [7, 3]이 나왔다. 여기서 문제는 [3, 7]과 대칭인 [7, 3]이었다. 필요하지 않은 연산을 더 하고 마지막 출력에 어려움을 주었기 때문에 for문의 조건을
+
+for (let j = 0; prime[j] < num / 2 + 1; j++) {
+로 주어 대칭되는 파티션을 제거하였다. 그랬더니 가장 마지막에 있는 파티션이 두 소수의 차이가 가장 작은 것이었고, 출력할 때 마지막 파티션을 출력해주어 해결하였다.
