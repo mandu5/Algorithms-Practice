c7d8bc09602eb2737d1ddcaf97372d08d2dd9ae8
gosmain <gosmain@naver.com>
2021-11-23 23:13:53 +0900

    BJ lv 11.1

---
+++ 백준 연습/11단계 브루트 포스/Baekjoon.js
@@ -0,0 +1,25 @@
+// 1번 (블랙잭) (세 장의 카드를 고르는 모든 경우를 고려하는 문제)
+let input = require('fs').readFileSync("/dev/stdin").toString().split("\n");
+input[0] = input[0].split(" ");
+const cards = input[1].split(" ").map((element) => Number(element));
+const N = Number(input[0][0]); // N은 총 카드의 개수
+const M = Number(input[0][1]); // M은 3개의 숫자의 합과 가장 가까운 목표 값
+let answer = 0;
+
+for(let i = 0; i < N; i++){
+   for(let j = i+1; j < N; j++){
+      for(let k = j+1; k < N; k++){
+         const sum = cards[i] + cards[j] + cards[k];
+         const gap = M - sum;
+         if(gap >= 0 && answer <= sum){
+            answer = sum;
+         }
+      }
+   }
+}
+console.log(answer);
+
+// 2번 (분해합) (모든 경우를 시도하여 N의 생성자를 구하는 문제)
+// 3번 (덩치) (모든 사람을 비교하여 덩치 등수를 구하는 문제)
+// 4번 (체스판 다시 칠하기) (체스판을 만드는 모든 경우를 시도하여 최적의 방법을 찾는 문제)
+// 5번 (영화감독 슘) (N번째 종말의 수가 나올 때까지 차례대로 시도하는 문제)
